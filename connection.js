d1064fe184cc8bc012041816b86147a9980998d6
Nathi Geigant <ngeigant@gmail.com>
2022-04-21 16:28:32 +0200

    I put up project tarsk. Is go.

---
+++ connection.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'rootuser',
+    password: 'sitar123',
+    database: 'tarsk'
+})
+
+module.exports = connection
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const connection = require('./connection')
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(cors())
+app.use(express.static('public'))
+
+app.listen(port, () => console.log(`${port}`))
+
+app.get('/species', (req, res) => {
+    let sql = "SELECT * FROM species"
+    connection.query(sql, function (error, results, fields) {
+        if (error) throw error
+        res.json(results)
+    })
+})
+
+app.get('/species/habitat', (req, res) => {
+    let sql = 
+    `SELECT speciesId,
+        speciesName,
+        GROUP_CONCAT(h.habitatType)
+    FROM species AS s
+        INNER JOIN speciesHabitat AS sh
+            ON s.speciesId = sh.speciesHabitatSId
+        INNER JOIN habitat AS h
+            ON sh.speciesHabitatHId = h.habitatId
+    GROUP BY speciesId`
+    connection.query(sql, function (error, results, fields) {
+        if (error) throw error
+        res.json(results)
+    })
+})

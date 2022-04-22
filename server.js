f8c72beff37c937a3d96cadc1874ae11c733a0a8
Adam Agerling <alltidvakenhehe@gmail.com>
2022-04-22 14:04:41 +0200

    fixed UUID param

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const connection = require('./connection')
-const port = 3000
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const connection = require('./connection');
+const port = 3000;
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(cors())
-app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.static('public'));
 
-app.listen(port, () => console.log(`${port}`))
+app.listen(port, () => console.log(`${port}`));
 
 app.get('/species', (req, res) => {
-    let sql = "SELECT * FROM species"
-    connection.query(sql, function (error, results, fields) {
-        if (error) throw error
-        res.json(results)
-    })
-})
+  let sql = 'SELECT * FROM species';
+  connection.query(sql, function (error, results, fields) {
+    if (error) throw error;
+    res.json(results);
+  });
+});
 
 app.get('/species/habitat', (req, res) => {
-    let sql = 
-    `SELECT speciesId,
+  let sql = `SELECT speciesId,
         speciesName,
         GROUP_CONCAT(h.habitatType)
     FROM species AS s
@@ -30,20 +29,28 @@ app.get('/species/habitat', (req, res) => {
             ON s.speciesId = sh.speciesHabitatSId
         INNER JOIN habitat AS h
             ON sh.speciesHabitatHId = h.habitatId
-    GROUP BY speciesId`
-    connection.query(sql, function (error, results, fields) {
-        if (error) throw error
-        res.json(results)
-    })
-})
+    GROUP BY speciesId`;
+  connection.query(sql, function (error, results, fields) {
+    if (error) throw error;
+    res.json(results);
+  });
+});
 
-app.get('/species/:speciesUUID', (req, res) => {
-    let speciesRov = req.params.speciesUUID
-    species.find({ speciesUUID: speciesRov}).toArray((err, items) => {
-        if (err) throw err
-        res.json({ species: items })
-    })
-})
+// app.get('/species/:speciesUUID', (req, res) => {
+//   let speciesRov = req.params.speciesUUID;
+//   species.find({ speciesUUID: speciesRov }).toArray((err, items) => {
+//     if (err) throw err;
+//     res.json({ species: items });
+//   });
+// });
+
+app.get('/species/:UUID', (req, res) => {
+  let sql = 'SELECT * FROM species WHERE speciesUUID = ?';
+  connection.query(sql, [req.params.UUID], function (error, results, fields) {
+    if (error) throw error;
+    res.json(results);
+  });
+});
 
 // app.get('/species/', (req,res) =>{
 //     let sql = "SELECT * FROM speciesUUID"

c067fdfd25f8a8b34b16900d1b9c98141bc98e52
Gustav Löwen-Åberg <gustav_lowen@hotmail.com>
2022-04-25 15:56:48 +0200

    Tried something

--- public/js/post-animal.js
+++ public/js/post-animal.js
@@ -1,53 +1,57 @@
-const elformCrocodile = document.querySelector('#formCrocodile');
-// const elspeciesUUID = document.querySelector('#speciesUUID');
-const elspeciesName = document.querySelector('#speciesName');
-const elspeciesFood = document.querySelector('#speciesFood');
+const elformCrocodile = document.querySelector('#formCrocodile')
+const elspeciesUUID = document.querySelector('#speciesUUID')
+const elspeciesName = document.querySelector('#speciesName')
+const elspeciesFood = document.querySelector('#speciesFood')
 // const elspeciesDevourHuman = document.querySelector('#speciesDevourHuman')
-const elspeciesLength = document.querySelector('#speciesLength');
-const elspeciesWeight = document.querySelector('#speciesWeight');
-// const elspeciesFamilyId = document.querySelector('#speciesFamilyId');
-const elOutput = document.querySelector('#output');
+const elspeciesLength = document.querySelector('#speciesLength')
+const elspeciesWeight = document.querySelector('#speciesWeight')
+const elspeciesFamilyId = document.querySelector('#speciesFamilyId')
+const elOutput = document.querySelector('#output')
 
-function newCrocodilia(event){
-    event.preventDefault();
-    let formCrocodile = elformCrocodile.value;
-    // let speciesUUID = elspeciesUUID.value;
-    let speciesName = elspeciesName.value;
-    let speciesFood = elspeciesFood.value;
-    // let speciesDevourHuman = elspeciesDevourHuman.value;
-    let speciesLength = elspeciesLength.value;
-    let speciesWeight = elspeciesWeight.value;
-    // let speciesFamilyId = elspeciesFamilyId.value;
-    alert('Test')
-    console.log(newCrocodilia);
+function newCrocodilia(event) {
+  event.preventDefault()
+  //   let speciesUUID = elspeciesUUID.value
+  let speciesName = elspeciesName.value
+  let speciesFood = elspeciesFood.value
+  // let speciesDevourHuman = elspeciesDevourHuman.value;
+  let speciesLength = elspeciesLength.value
+  let speciesWeight = elspeciesWeight.value
+  let speciesFamilyId = elspeciesFamilyId.value
+  alert('Test')
+  console.log(newCrocodilia)
 
-// Example POST method implementation:
-async function postData(url = '', data = {}) {
+  // Example POST method implementation:
+  async function postData(url = '', data = {}) {
     // Default options are marked with *
     const response = await fetch(url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-            'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *client
-        body: JSON.stringify({speciesName:speciesName, speciesFood:speciesFood, speciesLength:speciesLength, speciesWeight:speciesWeight}) // body data type must match "Content-Type" header
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *client
+      body: JSON.stringify({
+        speciesUUID: speciesUUID,
+        speciesName: speciesName,
+        speciesFood: speciesFood,
+        speciesLength: speciesLength,
+        speciesWeight: speciesWeight,
+        speciesFamilyId: speciesFamilyId
+      }) // body data type must match "Content-Type" header
+    })
+    return response.json() // parses JSON response into native JavaScript objects
+  }
+
+  postData('http://localhost:3000/species').then((data) => {
+    console.log(data) // JSON data parsed by `response.json()` call
+  })
 }
 
-postData('http://localhost:3000/species')
-    .then((data) => {
-        console.log(data); // JSON data parsed by `response.json()` call
-    });
-
-}
-
-elformCrocodile.addEventListener('submit', newCrocodilia, false);
+elformCrocodile.addEventListener('submit', newCrocodilia, false)
 
 /*
 https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
--- server.js
+++ server.js
@@ -1,50 +1,50 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const connection = require('./connection');
-const port = 3000;
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const connection = require('./connection')
+const port = 3000
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
-app.use(express.static('public'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+app.use(express.static('public'))
 
-app.listen(port, () => console.log(`${port}`));
+app.listen(port, () => console.log(`${port}`))
 
 app.get('/species', (req, res) => {
-  let sql = 'SELECT * FROM species';
+  let sql = 'SELECT * FROM species'
   connection.query(sql, function (error, results, fields) {
-    if (error) throw error;
-    res.json(results);
-  });
-});
+    if (error) throw error
+    res.json(results)
+  })
+})
 // Show Crocodiles
 app.get('/crocodile', (req, res) => {
-  let sql = 'SELECT speciesName FROM species WHERE speciesFamilyId = 1';
+  let sql = 'SELECT speciesName FROM species WHERE speciesFamilyId = 1'
   connection.query(sql, function (error, results, fields) {
-    if (error) throw error;
-    res.json(results);
-  });
-});
+    if (error) throw error
+    res.json(results)
+  })
+})
 
 // Show Alligators
 app.get('/alligator', (req, res) => {
-  let sql = 'SELECT speciesName FROM species WHERE speciesFamilyId = 2';
+  let sql = 'SELECT speciesName FROM species WHERE speciesFamilyId = 2'
   connection.query(sql, function (error, results, fields) {
-    if (error) throw error;
-    res.json(results);
-  });
-});
+    if (error) throw error
+    res.json(results)
+  })
+})
 
 // Show Gharials
 app.get('/gharial', (req, res) => {
-  let sql = 'SELECT speciesName FROM species WHERE speciesFamilyId = 3';
+  let sql = 'SELECT speciesName FROM species WHERE speciesFamilyId = 3'
   connection.query(sql, function (error, results, fields) {
-    if (error) throw error;
-    res.json(results);
-  });
-});
+    if (error) throw error
+    res.json(results)
+  })
+})
 
 app.get('/species/habitat', (req, res) => {
   let sql = `SELECT speciesId,
@@ -55,27 +55,34 @@ app.get('/species/habitat', (req, res) => {
             ON s.speciesId = sh.speciesHabitatSId
         INNER JOIN habitat AS h
             ON sh.speciesHabitatHId = h.habitatId
-    GROUP BY speciesId`;
+    GROUP BY speciesId`
   connection.query(sql, function (error, results, fields) {
-    if (error) throw error;
-    res.json(results);
-  });
-});
+    if (error) throw error
+    res.json(results)
+  })
+})
 
 app.get('/species/:UUID', (req, res) => {
-  let sql = 'SELECT * FROM species WHERE speciesUUID = ?';
+  let sql = 'SELECT * FROM species WHERE speciesUUID = ?'
   connection.query(sql, [req.params.UUID], function (error, results, fields) {
-    if (error) throw error;
-    res.json(results);
-  });
-});
+    if (error) throw error
+    res.json(results)
+  })
+})
 
-app.post('/species/', (req, res) =>  {
-  let sql ='CALL newCrocodilia(?, ?, ?, ?)'
-  let params =[req.body.speciesName, req.body.speciesFood, req.body.speciesLength, req.body.speciesWeight]
+app.post('/species/', (req, res) => {
+  let sql = 'CALL newCrocodilia(UUID(), ?, ?, ?, ?, ?)'
+  let params = [
+    req.body.speciesUUID,
+    req.body.speciesName,
+    req.body.speciesFood,
+    req.body.speciesLength,
+    req.body.speciesWeight,
+    req.body.speciesFamilyId
+  ]
   connection.query(sql, params, function (error, results, fields) {
-      if (error) throw error
-      res.json(results)
+    if (error) throw error
+    res.json(results)
   })
 })
 

e36f1ca3619136e2c46a60a8b8d96016332ff84a
Gustav Löwen-Åberg <gustav_lowen@hotmail.com>
2022-04-28 15:43:48 +0200

    Change back password

--- connection.js
+++ connection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql')
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'rootuser',
-    password: 'SiTar123!!',
-    database: 'tarsk'
+  host: 'localhost',
+  user: 'rootuser',
+  password: 'sitar123',
+  database: 'tarsk'
 })
 
 module.exports = connection
